766cff96f29cfe085b88b7ca9e12935a50db9e9b
code-sparsh <85060248+code-sparsh@users.noreply.github.com>
2021-06-06 14:36:22 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import configparser as cfg
+
+
+class telegram_chatbot():
+	def __init__(self, config):
+		self.token = self.read_token_from_config_file(config)
+		self.base = "https://api.telegram.org/bot{}/".format(self.token)
+
+	def get_updates(self, offset=None):
+		url = self.base + "getUpdates?timeout=100"
+		if offset:
+			url = url + "&offset={}".format(offset + 1)
+		r = requests.get(url)
+		return json.loads(r.content)
+
+	def send_message(self, msg, chat_id):
+		url = self.base + "sendMessage?chat_id={}&text={}".format(chat_id, msg)
+		if msg is not None:
+			requests.get(url)
+
+	def send_photo(self,link,msg,chat_id):
+	    url = self.base + "sendPhoto?chat_id={}&photo={}&caption={}".format("@abcd2649272",link,msg)
+	    if msg is not None:
+	        requests.get(url)
+
+	def read_token_from_config_file(self, config):
+		parser = cfg.ConfigParser()
+		parser.read(config)
+		return parser.get('creds', 'token')
+
